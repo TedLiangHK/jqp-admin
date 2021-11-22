09ca543dd755c9301260e85129a59abb11959433
hyz <792901324@qq.com>
2021-11-22 10:18:17 +0800

    初始化

---
+++ src/main/java/com/jqp/admin/JqpAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jqp.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqpAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JqpAdminApplication.class, args);
+    }
+
+}
