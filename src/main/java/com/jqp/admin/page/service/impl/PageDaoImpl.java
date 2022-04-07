df6d48386ed882df6459e619a0cabc5f2dc73523
Leo <root@workgroup.cn>
2022-04-07 20:56:44 +0800

    fix:修改page的code后没有删除旧的缓存

--- src/main/java/com/jqp/admin/page/service/impl/PageDaoImpl.java
+++ src/main/java/com/jqp/admin/page/service/impl/PageDaoImpl.java
@@ -22,63 +22,65 @@ public class PageDaoImpl implements PageDao {
     }
     @Override
     public Page get(String pageCode) {
-        Page page = jdbcService.findOne(Page.class,"code",pageCode);
-        if(page == null){
+        Page page = jdbcService.findOne(Page.class, "code", pageCode);
+        if (page == null) {
             return null;
         }
         this.get(page);
         return page;
     }
-    private void get(Page page){
-        List<PageResultField> pageResultFields = jdbcService.find(PageResultField.class,"pageId",page.getId());
+
+    @Override
+    public void delOldPageCache(String code) {
+    }
+
+    private void get(Page page) {
+        List<PageResultField> pageResultFields = jdbcService.find(PageResultField.class, "pageId", page.getId());
         page.setResultFields(pageResultFields);
 
-        List<PageQueryField> pageQueryFields = jdbcService.find(PageQueryField.class,"pageId",page.getId());
+        List<PageQueryField> pageQueryFields = jdbcService.find(PageQueryField.class, "pageId", page.getId());
         page.setQueryFields(pageQueryFields);
 
-        List<PageButton> pageButtons = jdbcService.find(PageButton.class,"pageId",page.getId());
+        List<PageButton> pageButtons = jdbcService.find(PageButton.class, "pageId", page.getId());
         page.setPageButtons(pageButtons);
 
-        List<PageRef> pageRefs = jdbcService.find(PageRef.class,"pageId",page.getId());
+        List<PageRef> pageRefs = jdbcService.find(PageRef.class, "pageId", page.getId());
         page.setPageRefs(pageRefs);
     }
+
     @Override
     @Transactional
     public void save(Page page) {
         jdbcService.saveOrUpdate(page);
-        jdbcService.delete("delete from page_result_field where page_id = ? ",page.getId());
+        jdbcService.delete("delete from page_result_field where page_id = ? ", page.getId());
         int seq = 0;
-        for (PageResultField field:
-                page.getResultFields()) {
+        for (PageResultField field : page.getResultFields()) {
             field.setId(null);
             field.setPageId(page.getId());
             field.setSeq(++seq);
             jdbcService.saveOrUpdate(field);
         }
-        jdbcService.delete("delete from page_query_field where page_id = ? ",page.getId());
+        jdbcService.delete("delete from page_query_field where page_id = ? ", page.getId());
         seq = 0;
-        for (PageQueryField field:
-                page.getQueryFields()) {
+        for (PageQueryField field : page.getQueryFields()) {
             field.setId(null);
             field.setPageId(page.getId());
             field.setSeq(++seq);
             jdbcService.saveOrUpdate(field);
         }
 
-        jdbcService.delete("delete from page_button where page_id = ? ",page.getId());
+        jdbcService.delete("delete from page_button where page_id = ? ", page.getId());
         seq = 0;
-        for (PageButton button:
-                page.getPageButtons()) {
+        for (PageButton button : page.getPageButtons()) {
             button.setId(null);
             button.setPageId(page.getId());
             button.setSeq(++seq);
             jdbcService.saveOrUpdate(button);
         }
 
-        jdbcService.delete("delete from page_ref where page_id = ? ",page.getId());
+        jdbcService.delete("delete from page_ref where page_id = ? ", page.getId());
         seq = 0;
-        for (PageRef ref:
-                page.getPageRefs()) {
+        for (PageRef ref : page.getPageRefs()) {
             ref.setId(null);
             ref.setPageId(page.getId());
             ref.setSeq(++seq);
