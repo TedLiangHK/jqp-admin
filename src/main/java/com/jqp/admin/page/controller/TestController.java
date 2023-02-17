1c67ed489ba86bb2ff117c307e1bb73883f71471
hyz <792901324@qq.com>
2023-02-17 15:06:52 +0800

    测试header

--- src/main/java/com/jqp/admin/page/controller/TestController.java
+++ src/main/java/com/jqp/admin/page/controller/TestController.java
@@ -12,11 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -33,5 +37,16 @@ public class TestController {
     @RequestMapping("/test")
     public Result test(){
         return Result.success("操作成功");
+    }
+    @RequestMapping("/headers")
+    public Result headers(HttpServletRequest request){
+        Enumeration<String> headerNames = request.getHeaderNames();
+        Map<String,Object> data = new HashMap<>();
+        while(headerNames.hasMoreElements()){
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            data.put(name,value);
+        }
+        return Result.success(data);
     }
 }
