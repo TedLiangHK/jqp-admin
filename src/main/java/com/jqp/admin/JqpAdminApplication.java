9d934abc5aa7542d8185a5ed98bf4aa82e802fb7
hyz <792901324@qq.com>
2023-03-03 14:09:46 +0800

    去掉spring-session-jdbc

--- src/main/java/com/jqp/admin/JqpAdminApplication.java
+++ src/main/java/com/jqp/admin/JqpAdminApplication.java
@@ -4,23 +4,18 @@ import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class},
     scanBasePackages = {
-        "com.jqp.admin.*.**",
+        "com.jqp.*.**",
         "org.activiti.rest.diagram.services"
     }
 )
 //超时时间两小时
-@EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 7200)
 @EnableScheduling
 public class JqpAdminApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JqpAdminApplication.class, args);
     }
-
 }
